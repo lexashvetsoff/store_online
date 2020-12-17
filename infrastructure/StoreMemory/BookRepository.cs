9ff5b5a3a6e7161b5171b857e9439fb805701652
lexashvetsoff <lexa.shvetsoff@yandex.ru>
2020-12-17 15:13:37 +0900

    Добавить описание и цену

--- infrastructure/StoreMemory/BookRepository.cs
+++ infrastructure/StoreMemory/BookRepository.cs
@@ -8,9 +8,10 @@ namespace StoreMemory
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "ISBN 12312-31231", "D. Knuth", "Art of Programming"),
-            new Book(2, "ISBN 12312-31232", "M. Fowler", "Refactoring"),
-            new Book(3, "ISBN 12312-31233", "B. Kernighan, D. Ritchie", "C Programming Language"),
+            new Book(1, "ISBN 12312-31231", "D. Knuth", "Art of Programming", "Opisanie", 7.19m),
+            new Book(2, "ISBN 12312-31232", "M. Fowler", "Refactoring", "Opisanie", 12.45m),
+            new Book(3, "ISBN 12312-31233", "B. Kernighan, D. Ritchie", "C Programming Language", 
+                    "Opisanie", 14.98m),
         };
 
         public Book[] GetAllByIsbn(string isbn)
